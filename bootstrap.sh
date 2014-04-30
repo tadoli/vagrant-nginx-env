f5a6985e512f178b895934c7113c4f792ec09c0e
이 승철 <tadoli2007@gmail.com>
2014-04-30 09:32:14 +0900

    change owner nginx conf, html

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 
 USER=vagrant
 USERHOME=/home/vagrant
-NODE_VERSION="0.10.26"
 NGINX_VERSION="1.4.7"
+NGINX_PATH=/data/program/nginx
 
 apt-get update
 apt-get install -y build-essential curl git vim nmap
@@ -19,13 +19,16 @@ wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar xzvf nginx-${NGINX_VERSION}.tar.gz
 
 cd nginx-${NGINX_VERSION}
-./configure --prefix=/data/program/nginx-${NGINX_VERSION} \
+./configure --prefix=${NGINX_PATH}-${NGINX_VERSION} \
 --error-log-path=/data/logs/nginx/error.log \
 --http-log-path=/data/logs/nginx/access.log \
 --with-http_ssl_module \
 --with-http_stub_status_module
 
 make && make install
-ln -s /data/program/nginx-${NGINX_VERSION} /data/program/nginx
+ln -s ${NGINX_PATH}-${NGINX_VERSION} ${NGINX_PATH}
 
-cd /data/program/nginx
+chown -R $USER $NGINX_PATH/conf
+chown -R $USER $NGINX_PATH/html
+
+cd ${NGINX_PATH}
